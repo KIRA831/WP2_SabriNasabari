f6d3ce82774d9419de12da668688dce5039e38b9
KIRA831 <Ayanokouji173@gmail.com>
2022-10-06 21:02:51 +0700

    Otaku

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Kiyotaka</li>
+ <li>Nama Belakang : Ayano</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan kemayoran</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Volly</li>
+ <li>Takraw</li>
+ </ul>
+</section>
